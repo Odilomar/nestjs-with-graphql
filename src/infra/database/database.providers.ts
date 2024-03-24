3b22555d586839f312d97f0004c49214394e69e6
Odilomar <BRSSI0007@totvspartners.com.br>
2024-03-24 12:00:22 -0300

    fix: use database info from env

--- src/infra/database/database.providers.ts
+++ src/infra/database/database.providers.ts
@@ -6,13 +6,13 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'admin',
-        password: 'admin',
-        database: 'nestjs_with_graphql_db',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT) || 5432,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: true,
+        synchronize: Boolean(process.env.TYPEORM_SYNCRONIZE) || false,
       });
 
       return dataSource.initialize();
